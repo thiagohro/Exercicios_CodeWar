702f9427e25a8468752cb1c005fdac6d9747997f
thiagohro <109321485+thiagohro@users.noreply.github.com>
2022-10-09 13:39:44 -0300

    1

---
+++ JavaScript/JavaScript_CodeWar_01.js
@@ -0,0 +1,21 @@
+//In this kata you will create a function that takes a list of non-negative integers and strings and 
+//returns a new list with the strings filtered out.
+
+  //E: numeros inteiros e letras
+  //Ação [1]: Numeros inteiros devem ser filtrados para non negative
+  //Ação [2]: Remover as strings
+  //Saida: Imprimir a nova lista com as strings de foranv
+
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+    
+    e.preventDefault();
+    
+    const name = document.querySelector("#aame");
+
+    const value = name.value;
+
+    console.log(value);
+
+});
